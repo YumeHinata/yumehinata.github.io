189d9a55e5661386d50706627d34b6388fc9e533
YumeHinata <yume3099729829@gmail.com>
2022-09-29 23:28:27 +0800

    9.29

--- js/manage.js
+++ js/manage.js
@@ -72,6 +72,77 @@ window.onload = function () {
             return String.fromCharCode('0x' + p1);
         }));
     }
+    // 生存markdown元素的函数
+    function edMarkdown(mdFile, eleM) {
+        $.get(mdFile, function (md) {
+            testEditor = editormd(eleM, {
+                // width: "90%",
+                height: 740,
+                path: '../editormd/lib/',
+                theme: "dark",
+                previewTheme: "dark",
+                editorTheme: "pastel-on-dark",
+                markdown: md,
+                codeFold: true,
+                //syncScrolling : false,
+                saveHTMLToTextarea: true,    // 保存 HTML 到 Textarea
+                searchReplace: true,
+                //watch : false,                // 关闭实时预览
+                htmlDecode: "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启    
+                //toolbar  : false,             //关闭工具栏
+                //previewCodeHighlight : false, // 关闭预览 HTML 的代码块高亮，默认开启
+                emoji: true,
+                taskList: true,
+                tocm: true,         // Using [TOCM]
+                tex: true,                   // 开启科学公式TeX语言支持，默认关闭
+                flowChart: true,             // 开启流程图支持，默认关闭
+                sequenceDiagram: true,       // 开启时序/序列图支持，默认关闭,
+                //dialogLockScreen : false,   // 设置弹出层对话框不锁屏，全局通用，默认为true
+                //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为true
+                //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为true
+                //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为0.1
+                //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
+                imageUpload: false,
+                imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+                imageUploadURL: "./php/upload.php",
+                onload: function () {
+                    console.log('onload', this);
+                    //this.fullscreen();
+                    //this.unwatch();
+                    //this.watch().fullscreen();
+                    //this.setMarkdown("#PHP");
+                    //this.width("100%");
+                    //this.height(480);
+                    //this.resize("100%", 640);
+                }
+            });
+            var pushE = document.getElementById("push");
+            var writingE = document.getElementById(eleM);
+            writingE.style.height = window.innerHeight - 2 + "px";
+            writingE.style.width = main.offsetWidth - 270 + "px";
+            pushE.style.height = window.innerHeight + "px";
+            // 加载默认摘要内容，摘要默认为文章前30字+省略号
+            var writingText = document.getElementsByClassName("editormd-markdown-textarea")[0];
+            var summary = document.getElementsByClassName("summary")[0];
+            // console.log(writingText);
+            var ThereAreText = 0;
+            summary.onfocus = function () {
+                // console.log(ThereAreText)
+                summary.onchange = function () {
+                    // 判断人工输入
+                    ThereAreText = 1;
+                    // console.log(ThereAreText)
+                }
+            }
+            $(".editormd-markdown-textarea").on('DOMNodeInserted', function () {
+                let text = writingText.innerHTML;
+                // console.log(text);
+                if ((summary.value.length < 30) && (ThereAreText == 0)) {
+                    summary.value = text;
+                }
+            });
+        });
+    }
     // 图片上传github
     async function pushImage(pcc) {
         // 获取图片目录
@@ -103,7 +174,7 @@ window.onload = function () {
                 let x = imgSearch[Y][M][D][i];
                 if (((typeof x) != undefined) && ((typeof x) != "undefined")) {
                     i++
-                }else{
+                } else {
                     break
                 }
             } catch {
@@ -169,74 +240,7 @@ window.onload = function () {
     var testEditor;
     var xNewMd = "./config/new.md";
     $(function () {
-        $.get(xNewMd, function (md) {
-            testEditor = editormd("writing", {
-                // width: "90%",
-                height: 740,
-                path: '../editormd/lib/',
-                theme: "dark",
-                previewTheme: "dark",
-                editorTheme: "pastel-on-dark",
-                markdown: md,
-                codeFold: true,
-                //syncScrolling : false,
-                saveHTMLToTextarea: true,    // 保存 HTML 到 Textarea
-                searchReplace: true,
-                //watch : false,                // 关闭实时预览
-                htmlDecode: "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启    
-                //toolbar  : false,             //关闭工具栏
-                //previewCodeHighlight : false, // 关闭预览 HTML 的代码块高亮，默认开启
-                emoji: true,
-                taskList: true,
-                tocm: true,         // Using [TOCM]
-                tex: true,                   // 开启科学公式TeX语言支持，默认关闭
-                flowChart: true,             // 开启流程图支持，默认关闭
-                sequenceDiagram: true,       // 开启时序/序列图支持，默认关闭,
-                //dialogLockScreen : false,   // 设置弹出层对话框不锁屏，全局通用，默认为true
-                //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为true
-                //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为true
-                //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为0.1
-                //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
-                imageUpload: false,
-                imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
-                imageUploadURL: "./php/upload.php",
-                onload: function () {
-                    console.log('onload', this);
-                    //this.fullscreen();
-                    //this.unwatch();
-                    //this.watch().fullscreen();
-                    //this.setMarkdown("#PHP");
-                    //this.width("100%");
-                    //this.height(480);
-                    //this.resize("100%", 640);
-                }
-            });
-            var pushE = document.getElementById("push");
-            var writingE = document.getElementById("writing");
-            writingE.style.height = window.innerHeight - 2 + "px";
-            writingE.style.width = main.offsetWidth - 270 + "px";
-            pushE.style.height = window.innerHeight + "px";
-            // 加载默认摘要内容，摘要默认为文章前30字+省略号
-            var writingText = document.getElementsByClassName("editormd-markdown-textarea")[0];
-            var summary = document.getElementsByClassName("summary")[0];
-            // console.log(writingText);
-            var ThereAreText = 0;
-            summary.onfocus = function () {
-                // console.log(ThereAreText)
-                summary.onchange = function () {
-                    // 判断人工输入
-                    ThereAreText = 1;
-                    // console.log(ThereAreText)
-                }
-            }
-            $(".editormd-markdown-textarea").on('DOMNodeInserted', function () {
-                let text = writingText.innerHTML;
-                // console.log(text);
-                if ((summary.value.length < 30) && (ThereAreText == 0)) {
-                    summary.value = text;
-                }
-            });
-        });
+        edMarkdown(xNewMd,"writing");
     });
     // 改变markdown大小
     let main = document.getElementById("main");
@@ -247,11 +251,16 @@ window.onload = function () {
         let writing = document.getElementById("writing");
         var pushE = document.getElementById("push");
         var writingE = document.getElementById("writing");
+        let oldWriting = document.getElementById("main-oldWriting");
+        let oldPush = document.getElementById("main-oldPush");
         main.style.width = window.innerWidth - 200 + "px";
         main.style.height = window.innerHeight + "px";
         writing.style.height = window.innerHeight - 2 + "px";
         pushE.style.height = window.innerHeight + "px"
-        writingE.style.width = main.offsetWidth - pushE.offsetWidth + "px"
+        oldPush.style.height = window.innerHeight + "px"
+        writingE.style.width = main.offsetWidth - oldPush.offsetWidth + "px"
+        oldWriting.style.height = window.innerHeight - 2 + "px";
+        oldWriting.style.width = main.offsetWidth - oldPush.offsetWidth + "px"
     }
     // 封面加载功能
     var coverFile = document.getElementById("coverFile");
@@ -313,7 +322,7 @@ window.onload = function () {
                     if (((typeof x) != undefined) && ((typeof x) != "undefined")) {
                         // break
                         i++
-                    }else{
+                    } else {
                         break
                     }
                     // console.log(x);
@@ -360,7 +369,7 @@ window.onload = function () {
                 let authorC = document.getElementById("author").value;
                 // 读取并写入index.json
                 let NewTitle = document.getElementById("newTitle").value;
-                let pushObj = { "year": Year, "month": Month, "day": Day, "paper": paperNum, "title": NewTitle, "image": coverUrl ,"summary": pushSummary };
+                let pushObj = { "year": Year, "month": Month, "day": Day, "paper": paperNum, "title": NewTitle, "image": coverUrl, "summary": pushSummary };
                 let indexIndex = indexJson.index;
                 indexIndex.push(pushObj);
                 let index = indexIndex;
@@ -372,7 +381,8 @@ window.onload = function () {
                                 [paperNum]: {
                                     "title": NewTitle,
                                     "image": coverUrl,
-                                    "author": authorC
+                                    "author": authorC,
+                                    "summary": pushSummary
                                 }
                             }
                         }
@@ -383,7 +393,8 @@ window.onload = function () {
                             [paperNum]: {
                                 "title": NewTitle,
                                 "image": coverUrl,
-                                "author": authorC
+                                "author": authorC,
+                                "summary": pushSummary
                             }
                         }
                     }
@@ -392,14 +403,16 @@ window.onload = function () {
                         [paperNum]: {
                             "title": NewTitle,
                             "image": coverUrl,
-                            "author": authorC
+                            "author": authorC,
+                            "summary": pushSummary
                         }
                     }
                 } else if (indexSearch[Year][Month][Day][paperNum] == undefined) {
                     indexSearch[Year][Month][Day][paperNum] = {
                         "title": NewTitle,
                         "image": coverUrl,
-                        "author": authorC
+                        "author": authorC,
+                        "summary": pushSummary
                     }
                 }
                 // 下面是上傳內容
@@ -429,35 +442,93 @@ window.onload = function () {
     // 打开编辑页，并实现编辑页功能
     // 点击sidebar-alter（编辑）后隐藏writing和push,显示main-alter
     let sidebarAlter = document.getElementsByClassName("sidebar-alter")[0];
-    sidebarAlter.onclick = function(){
+    sidebarAlter.onclick = function () {
         let mainWriting = document.getElementById("writing");
         let mainPush = document.getElementById("push");
         let mainAlter = document.getElementById("main-alter");
+        let mainOldPush = document.getElementById("main-oldPush");
+        let mainOldWriting = document.getElementById("main-oldWriting");
+        mainOldPush.innerHTML = '';
+        mainOldWriting.innerHTML = '';
         mainWriting.style.display = "none";
         mainPush.style.display = "none";
         mainAlter.style.display = "block"
+        mainOldPush.style.display = "none";
+        mainOldWriting.style.display = "none";
         // 点击后加载文章列表
-        useJson("../paper/index.json",function(){
+        useJson("../paper/index.json", function () {
             var paperIndex = json.index;
             let PaperList = document.getElementsByClassName("paper-list")[0];
-            for(let i=0;i < paperIndex.length;i++){
+            PaperList.innerHTML = "";
+            for (let i = 0; i < paperIndex.length; i++) {
                 // 获取日期、标题
                 let paperDate = paperIndex[i].year + "-" + paperIndex[i].month + "-" + paperIndex[i].day;
                 let paperUrl = paperIndex[i].year + "-" + paperIndex[i].month + "-" + paperIndex[i].day + "-" + paperIndex[i].paper;
-                PaperList.innerHTML += '<li class="list-p ' + paperDate + '"><span class="' + paperIndex[i].year + '">' + paperIndex[i].year + '</span><span class="' + paperIndex[i].year+'-'+paperIndex[i].month + '">' + paperIndex[i].month + '</span>' + '<span class="' + paperDate + '">' + paperIndex[i].day + '</span>' + '<a href="../page/paper.html?number=' + paperUrl + '"class="paper-title">' + paperIndex[i].title + '</a><a href="javascript:;">编辑</a> / <a href="javascript:;">删除</a></li>';
+                PaperList.innerHTML += '<li class="list-p ' + paperDate + '"><span class="' + paperIndex[i].year + '">' + paperIndex[i].year + '</span><span class="' + paperIndex[i].year + '-' + paperIndex[i].month + '">' + paperIndex[i].month + '</span>' + '<span class="' + paperDate + '">' + paperIndex[i].day + '</span>' + '<a href="../page/paper.html?number=' + paperUrl + '"class="paper-title">' + paperIndex[i].title + '</a><a href="javascript:;" class="paper-alter ' + paperUrl + '">编辑</a><span>/</span><a href="javascript:;" class="paper-delete ' + paperUrl + '">删除</a></li>';
+
             }
-            console.log(paperIndex)
+            // console.log(paperIndex)
+            // 编辑文章按钮
+            var paperAlter = document.getElementsByClassName("paper-alter");
+            for (let i = 0; i < paperAlter.length; i++) {
+                paperAlter[i].onclick = async function () {
+                    // console.log(this);
+                    // 隐藏main-alter
+                    let mainAlter = document.getElementById("main-alter");
+                    mainAlter.style.display = "none";
+                    // 调用markdown并载入相应的文章
+                    // 获取classname中的日期并获取文件路径
+                    let mdUrl = this.className;
+                    mdUrl = "../paper/" + mdUrl.replace("paper-alter ","").replace(/-/g,"/") + ".md";
+                    // console.log(mdUrl);
+                    edMarkdown(mdUrl,"main-oldWriting");
+                    // 生成main-oldPush
+                    let mainOldPush = document.getElementById("main-oldPush");
+                    let pushE = document.getElementById("push");
+                    mainOldPush.innerHTML = '<h4>标题:</h4><input class="oldTitle"><h4>封面：</h4><div class="oldCover"></div><input type="file" name="" class="coverFile"><h4>摘要:</h4><textarea name="" cols="30" rows="10" class="summary"></textarea><h4>作者:</h4><input class="author"><div class="commit">提交</div>';
+                    // 改变标题、封面、作者、摘要元素中的内容
+                    var paperIndexSearch = await useJson("../paper/index.json",function(){}).search;
+                    let Year = this.className.replace("paper-alter ","").match(/[0-9]{4}/)[0];
+                    let Month = this.className.replace("paper-alter ","").replace(/-/g,"").match(/[0-9]{6}/)[0].replace(Year,"");
+                    let Day = this.className.replace("paper-alter ","").replace(/-/g,"").match(/[0-9]{8}/)[0].replace(Year,"").replace(Month,"");
+                    let Page = this.className.replace("paper-alter ","").replace(/-/g,"").replace(Year,"").replace(Month,"").replace(Day,"");
+                    let oldTitleContent = paperIndexSearch[Year][Month][Day][Page].title;
+                    let oldPushTitle = document.getElementById("main-oldPush").getElementsByClassName("oldTitle")[0];
+                    oldPushTitle.value = oldTitleContent;
+                    let oldCoverContent = paperIndexSearch[Year][Month][Day][Page].image;
+                    let oldPushCover = document.getElementById("main-oldPush").getElementsByClassName("oldCover")[0];
+                    oldPushCover.style.backgroundImage = "url('" + oldCoverContent + "')";
+                    let oldSummaryContent = paperIndexSearch[Year][Month][Day][Page].summary;
+                    let oldPushSummary = document.getElementById("main-oldPush").getElementsByClassName("summary")[0];
+                    if((typeof oldSummaryContent=="undefined")){
+                        oldPushSummary.value = "";
+                    }else{
+                        oldPushSummary.value = oldSummaryContent;
+                    }
+                    
+                    console.log(oldCoverContent);
+                    // 显示oldWriting和oldPush
+                    let mainOldWriting = document.getElementById("main-oldWriting");
+                    mainOldPush.style.display = "block";
+                    mainOldWriting.style.display = "block";
+                    
+                }
+            }
         });
     }
 
-    // 点击sidebar-writing（写作）后恢复writing和push,隐藏main-alter
+    // 点击sidebar-writing（写作）后恢复writing和push,隐藏main-alter,main-oldWriting,main-oldWriting
     let sidebarWriting = document.getElementsByClassName("sidebar-writing")[0];
-    sidebarWriting.onclick = function(){
+    sidebarWriting.onclick = function () {
         let mainWriting = document.getElementById("writing");
         let mainPush = document.getElementById("push");
         let mainAlter = document.getElementById("main-alter");
+        let mainOldPush = document.getElementById("main-oldPush");
+        let mainOldWriting = document.getElementById("main-oldWriting");
         mainWriting.style.display = "block";
         mainPush.style.display = "block";
         mainAlter.style.display = "none"
+        mainOldPush.style.display = "none";
+        mainOldWriting.style.display = "none";
     }
 }
