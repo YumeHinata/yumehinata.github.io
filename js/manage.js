7a85fdc43b2666b3b5cdb1cd2a083de0f61caf19
YumeHinata <yume3099729829@gmail.com>
2022-10-05 13:57:39 +0800

    xiuz

--- js/manage.js
+++ js/manage.js
@@ -613,9 +613,9 @@ window.onload = function () {
                             let pushPaperIndexContent = turnBase64(paperIndexContent);
                             // console.log(paperIndexContent);
                             // 提交修改后的md文件、文章目录json
-                            // octokitPush(readToken(),paperNumber.replace("../",""),"3099729829@qq.com",mdSha.sha,pushOldWritingContent);
-                            // octokitPush(readToken(),"paper/index.json","3099729829@qq.com",paperIndexSha,pushPaperIndexContent);
-                            console.log(paperIndexContent);
+                            octokitPush(readToken(),paperNumber.replace("../",""),"3099729829@qq.com",mdSha.sha,pushOldWritingContent);
+                            octokitPush(readToken(),"paper/index.json","3099729829@qq.com",paperIndexSha,pushPaperIndexContent);
+                            // console.log(paperIndexContent);
                         }
                     }
                 }
